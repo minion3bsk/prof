dcbaee032d4df2e1c2382ddd5f6d75dfea7930a9
EC2 Default User <ec2-user@ip-172-31-40-40.ap-northeast-1.compute.internal>
2020-07-04 01:57:04 +0000

    active_strage　完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,6 @@ class UsersController < ApplicationController
   private
   
   def update_params
-    params.require(:user).permit(:family_name, :family_name_kana, :first_name, :first_name_kana)
+    params.require(:user).permit(:family_name, :family_name_kana, :first_name, :first_name_kana, :image)
   end
 end
